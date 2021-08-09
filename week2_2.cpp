30827868d4f98fedcb5695e8471c885256b91666
NRdeARK <85008989+NRdeARK@users.noreply.github.com>
2021-08-09 16:48:32 +0700

    Initial commit

---
+++ week2_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a,max,min;
+    cin >> a;
+    max=a;
+    min=a;
+    for (int i = 0; i < 2; i++)
+    {
+        cin >> a;
+        if (a>max)
+        {
+            max=a;
+        }
+        if (a<max)
+        {
+            min=a;
+        }
+    }
+    cout<<min+max;
+}
+
